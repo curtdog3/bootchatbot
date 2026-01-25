026be27513fa7a94c12fccb4e2da85e8d8e6a0e0
curtdog3 <curtismathson@gmail.com>
2026-01-25 15:31:26 -0600

    add token counts

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    usage_metadata=response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("failed API request")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
     print(response.text)
 
 
