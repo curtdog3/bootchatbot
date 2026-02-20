7c6c440f082e0e7a98d95abe747d74c63c53093c
curtdog3 <curtismathson@gmail.com>
2026-02-20 15:40:38 -0600

    add more schemas and prompts

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=None):
     try:
@@ -34,3 +35,23 @@ def run_python_file(working_directory, file_path, args=None):
         return "\n".join(output)
     except Exception as e:
         return f"Error: executing Python file: {e}"
+    
+schema_run_python_file = types.FunctionDeclaration(
+name="run_python_file",
+description="Runs a Python file",
+parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+        "args": types.Schema(
+            type=types.Type.ARRAY,
+            description="Optional command-line arguments to pass to the Python file",
+            items=types.Schema(type=types.Type.STRING),
+            ),
+        "file_path": types.Schema(
+            type=types.Type.STRING,
+            description="Path to file, relative to the working directory",
+            ),
+        },
+        required=["file_path"]
+    ),
+)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def write_file(working_directory, file_path, content):
     try:
@@ -16,3 +17,22 @@ def write_file(working_directory, file_path, content):
             return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: {e}"
+    
+schema_write_file = types.FunctionDeclaration(
+name="write_file",
+description="Writes characters into specified file path",
+parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+        "file_path": types.Schema(
+            type=types.Type.STRING,
+            description="Path to file, relative to the working directory",
+            ),
+        "content": types.Schema(
+            type=types.Type.STRING,
+            description="text inside file"
+            ),
+        },
+        required=["file_path", "content"]
+    ),
+)
