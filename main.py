dc0f75e0e045647fa1097d0392c509c37d3fafb7
curtdog3 <curtismathson@gmail.com>
2026-02-20 16:40:31 -0600

    add read.me, define call_funtion, add call to main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 import argparse
 from google.genai import types
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 def main():
     load_dotenv()
@@ -32,8 +32,19 @@ def main():
         print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
         print(f"Response tokens: {usage_metadata.candidates_token_count}")
     if response.function_calls:
+        function_responses = []
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+            if not function_call_result.parts:
+                raise Exception("Nothing in the list!")
+            first_item = function_call_result.parts[0]
+            if first_item.function_response is None:
+                raise Exception("Nested data is missing!")
+            if first_item.function_response.response is None:
+                raise Exception("Empty!")
+            function_responses.append(first_item)
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(response.text)
 
