30995dbf121fcc97b21cea71df3dddf9cf357123
curtdog3 <curtismathson@gmail.com>
2026-02-21 16:37:05 -0600

    add agency to the agent

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from google.genai import types
 from prompts import system_prompt
 from functions.call_function import available_functions, call_function
+import sys
 
 def main():
     load_dotenv()
@@ -16,37 +17,46 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-
+    
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-        )
-    usage_metadata=response.usage_metadata
-    if usage_metadata is None:
-        raise RuntimeError("failed API request")
-    if args.verbose is True:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
-    if response.function_calls:
-        function_responses = []
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if not function_call_result.parts:
-                raise Exception("Nothing in the list!")
-            first_item = function_call_result.parts[0]
-            if first_item.function_response is None:
-                raise Exception("Nested data is missing!")
-            if first_item.function_response.response is None:
-                raise Exception("Empty!")
-            function_responses.append(first_item)
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
+    
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+            )
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        usage_metadata=response.usage_metadata
+        if usage_metadata is None:
+            raise RuntimeError("failed API request")
+        if args.verbose is True:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            function_responses = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
+                if not function_call_result.parts:
+                    raise Exception("Nothing in the list!")
+                first_item = function_call_result.parts[0]
+                if first_item.function_response is None:
+                    raise Exception("Nested data is missing!")
+                if first_item.function_response.response is None:
+                    raise Exception("Empty!")
+                function_responses.append(first_item)
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_responses))
+        else:
+            return print(response.text)
+        
+    print("Maximum iterations met")
+    sys.exit(1) 
 
 
 if __name__ == "__main__":
